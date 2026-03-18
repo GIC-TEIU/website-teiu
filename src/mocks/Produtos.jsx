e7831f53da7df627f3226761678476c4b9a88848
Ana Luiza Vasconcelos <desenvolvimento.software2@teiu.com.br>
2026-03-18 12:06:02 -0300

    feat: adiciona arquivo de dados mockados aos mocks em produtos

---
+++ src/mocks/Produtos.jsx
@@ -0,0 +1,22 @@
+export const produtosTeiu = [
+  {
+    id: 1,
+    title: "Lava Roupas Líquido",
+    imageUrl: "/assets/img/lava-roupas.png" 
+  },
+  {
+    id: 2,
+    title: "Água Sanitária",
+    imageUrl: "/assets/img/agua.png"
+  },
+  {
+    id: 3,
+    title: "Desinfetante Lavanda",
+    imageUrl: "/assets/img/desinfetante.png"
+  },
+  {
+    id: 4,
+    title: "Vatz Original",
+    imageUrl: "/assets/img/vatz1.png"
+  }
+];

39d8518282f5dc09519aee8bd7c9925199128324
Ana Luiza Vasconcelos <desenvolvimento.software2@teiu.com.br>
2026-03-18 16:58:01 -0300

    feat: adiciona video na pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar"
 import ProductsHome from "./pages/ProductsHome"
 import Hero from "./pages/Hero"
 import Footer from "./components/Footer"
+import InstitutionalVideo from "./pages/VideoPage"
 
 function App() {
   return (
@@ -10,7 +11,10 @@ function App() {
       <main>
         <Hero />
         <ProductsHome />
+        <InstitutionalVideo />
+        <div className="bg-linear-to-r from-cyan-500 to-blue-500">
         <Footer/>
+        </div>
       </main>
     </>
   )

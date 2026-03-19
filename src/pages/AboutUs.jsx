3d4699d5acd070891f90a590d279b98b83b83e2d
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-03-19 14:29:59 -0300

    feat: adiciona página about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,23 @@
 import Navbar from "../components/navbar";
+import BannerMaster from "../sections/BannerMaster";
+import FounderSection from "../sections/FounderSection.jsx";
+import OurFacilities from "../sections/OurFacilities";
+import OurHistory from "../sections/OurHistory";
 
 function AboutUs() {
     return ( 
     <>
         <Navbar/>
+        <BannerMaster
+        title={'Lorem Impsum Dolor'}
+        text={' Lorem ipsum dolor sit, amet consectetur adipisicing elit.  At repellendus beatae distinctio suscipit esse, eveniet, animi laboriosam dolorum, sit repudiandae obcaecati accusamus rerum soluta nam perspiciatis quas voluptatem. Tempora, veniam!'}
+        />
+        <OurFacilities/>
+        <OurHistory/>
+        <FounderSection/>
+        <div className="bg-gradient-to-br from-[#02D1FF] to-[#074B9A]">
+
+        </div>
     </>
     );
 }

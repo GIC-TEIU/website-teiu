d22759fe8d08db926b641cbf63424d3eb0399511
stefani-teiu <desenvolvimento.web@teiu.com.br>
2026-03-19 16:47:26 -0300

    refactor(core): atualizar dados dos produtos com categorias e marcas

--- src/mocks/Products.js
+++ src/mocks/Products.js
@@ -1,87 +1,137 @@
 export const produtosTeiu = [
-  {
-    id: 2,
-    title: "Água Sanitária",
-    imageUrl: "/assets/img/agua.png"
-  },
-  {
-    id: 3,
-    title: "Desinfetante",
-    imageUrl: "/assets/img/desinfetante.png"
-  },
-  {
-    id: 4,
-    title: "Vatz Original",
-    imageUrl: "/assets/img/vatz1.png"
-  },
-  {
-    id: 5,
-    title: "Gel Lavanda",
-    imageUrl: "/assets/img/gel.png"
-  },
-  {
-    id: 6,
-    title: "Sabonete Líquido",
-    imageUrl: "/assets/img/sabonete-liquido.png"
-  },
-  {
-    id: 7,
-    title: "Amaciante Amor 1L",
-    imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 1L.png"
-  },
-  {
-    id: 8,
-    title: "Amaciante Amor 2L",
-    imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 2L.png"
-  },
-  {
-    id: 9,
-    title: "Amaciante Amor 3L",
-    imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 3L.png"
-  },
-  {
-    id: 10,
-    title: "Lava Louças Maçã 500ML",
-    imageUrl: "/assets/img/TEIÚ - LAVA LOUÇAS MAÇÃ 500ML.png"
-  },
-  {
-    id: 11,
-    title: "Lava Louças Maçã 2L",
-    imageUrl: "/assets/img/TEIÚ - LAVA LOUÇAS MAÇÃ 2L.png"
-  },
-  {
-    id: 12,
-    title: "Lava Roupas Líquido Perfumes 1L",
-    imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 1L.png"
-  },
-  {
-    id: 13,
-    title: "Lava Roupas Líquido Perfumes 3L",
-    imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 3L.png"
-  },
-  {
-    id: 14,
-    title: "Lava Roupas Líquido Perfumes 5L",
-    imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 5L.png"
-  },
-  {
-    id: 15,
-    title: "Limpador Perfumado Felicitá 500ML",
-    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO FELICITÁ 500ML.png"
-  },
-  {
-    id: 16,
-    title: "Limpador Perfumado Felicitá 1L",
-    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO FELICITÁ 1L.png"
-  },
-  {
-    id: 17,
-    title: "Limpador Perfumado Intimitá 500ML",
-    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO INTIMITÁ 500ML.png"
-  },
-  {
-    id: 18,
-    title: "Limpador Perfumado Intimitá 1L",
-    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO INTIMITÁ 1L.png"
-  }
+  { id: 2,
+     title: "Água Sanitária",
+     imageUrl: "/assets/img/agua.png",
+     category: "Limpeza Pesada",
+     brand: "Teiú",
+     fragrance: "Original",
+     volume: "1L" },
+  
+  { id: 3,
+     title: "Desinfetante Lavanda",
+     imageUrl: "/assets/img/desinfetante.png",
+     category: "Desinfetantes",
+     brand: "Teiú",
+     fragrance: "Lavanda",
+     volume: "1L" },
+  
+  { id: 4,
+     title: "Vatz Original",
+     imageUrl: "/assets/img/vatz1.png",
+     category: "Multiuso",
+     brand: "Vatz",
+     fragrance: "Original",
+     volume: "500ml" },
+  
+  { id: 5,
+     title: "Gel Lavanda",
+     imageUrl: "/assets/img/gel.png",
+     category: "Limpeza Específica",
+     brand: "Teiú",
+     fragrance: "Lavanda",
+     volume: "500ml" },
+  
+  { id: 6,
+     title: "Sabonete Líquido",
+     imageUrl: "/assets/img/sabonete-liquido.png",
+     category: "Higiene",
+     brand: "Teiú",
+     fragrance: "Marine",
+     volume: "500ml" },
+  
+  { id: 7,
+     title: "Amaciante Amor 1L",
+     imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 1L.png",
+     category: "Roupas",
+     brand: "Teiú",
+     fragrance: "Amor",
+     volume: "1L" },
+  
+  { id: 8,
+     title: "Amaciante Amor 2L",
+     imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 2L.png",
+     category: "Roupas",
+     brand: "Teiú",
+     fragrance: "Amor",
+     volume: "2L" },
+  
+  { id: 9,
+     title: "Amaciante Amor 3L",
+     imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 3L.png",
+     category: "Roupas",
+     brand: "Teiú",
+     fragrance: "Amor",
+     volume: "3L" },
+  
+  { id: 10,
+     title: "Lava Louças Maçã 500ML",
+     imageUrl: "/assets/img/TEIÚ - LAVA LOUÇAS MAÇÃ 500ML.png",
+     category: "Cozinha",
+     brand: "Teiú",
+     fragrance: "Maçã",
+     volume: "500ml" },
+  
+  { id: 11,
+     title: "Lava Louças Maçã 2L",
+     imageUrl: "/assets/img/TEIÚ - LAVA LOUÇAS MAÇÃ 2L.png",
+     category: "Cozinha",
+     brand: "Teiú",
+     fragrance: "Maçã",
+     volume: "2L" },
+  
+  { id: 12,
+     title: "Lava Roupas Líquido Perfumes 1L",
+     imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 1L.png",
+     category: "Roupas",
+     brand: "Teiú",
+     fragrance: "Perfumes",
+     volume: "1L" },
+  
+  { id: 13,
+     title: "Lava Roupas Líquido Perfumes 3L",
+     imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 3L.png",
+     category: "Roupas",
+     brand: "Teiú",
+     fragrance: "Perfumes",
+     volume: "3L" },
+  
+  { id: 14,
+     title: "Lava Roupas Líquido Perfumes 5L",
+     imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 5L.png",
+     category: "Roupas",
+     brand: "Teiú",
+     fragrance: "Perfumes",
+     volume: "5L" },
+  
+  { id: 15,
+     title: "Limpador Perfumado Felicitá 500ML",
+     imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO FELICITÁ 500ML.png",
+     category: "Limpeza Perfumada",
+     brand: "Teiú",
+     fragrance: "Felicitá",
+     volume: "500ml" },
+  
+  { id: 16,
+     title: "Limpador Perfumado Felicitá 1L",
+     imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO FELICITÁ 1L.png",
+     category: "Limpeza Perfumada",
+     brand: "Teiú",
+     fragrance: "Felicitá",
+     volume: "1L" },
+  
+  { id: 17,
+     title: "Limpador Intimitá 500ML",
+     imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO INTIMITÁ 500ML.png",
+     category: "Limpeza Perfumada",
+     brand: "Teiú",
+     fragrance: "Intimitá",
+     volume: "500ml" },
+  
+  { id: 18,
+     title: "Limpador Intimitá 1L",
+     imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO INTIMITÁ 1L.png",
+     category: "Limpeza Perfumada",
+     brand: "Teiú",
+     fragrance: "Intimitá",
+     volume: "1L" }
 ];

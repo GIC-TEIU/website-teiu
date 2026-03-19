97950b823adfdc367280accca75cd1b3f0fd7a61
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-03-19 14:29:48 -0300

    feat: adiciona sessão do fundador

---
+++ src/sections/FounderSection.jsx
@@ -0,0 +1,25 @@
+function FounderSection() {
+    return (
+      <section className="w-full py-16 px-4 sm:px-8 lg:px-[150px] flex justify-center">
+        
+        <div className="relative max-w-[50%] ">
+  
+          {/* Imagem de fundo */}
+          <img
+            src="/assets/img/founder.jpg"
+            alt="Fundador"
+            className=""
+          />
+  
+        <div className="flex flex-col items-center text-center">
+        <h4 className="font-bold mt-[20px] mb-[5px]" >Risus commodo viverra maecenas accumsan lacus vel.</h4>
+        <p>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel.</p>
+      </div>
+          
+        </div>
+  
+      </section>
+    );
+  }
+  
+  export default FounderSection;

2cf91fd27cd122024754fb8ac410423777bd2d8a
stefani-teiu <desenvolvimento.web@teiu.com.br>
2026-03-19 15:39:17 -0300

    feat(mocks): adiciona nova estrutura de dados em js para os produtos

---
+++ src/mocks/Products.js
@@ -0,0 +1,87 @@
+export const produtosTeiu = [
+  {
+    id: 2,
+    title: "Água Sanitária",
+    imageUrl: "/assets/img/agua.png"
+  },
+  {
+    id: 3,
+    title: "Desinfetante",
+    imageUrl: "/assets/img/desinfetante.png"
+  },
+  {
+    id: 4,
+    title: "Vatz Original",
+    imageUrl: "/assets/img/vatz1.png"
+  },
+  {
+    id: 5,
+    title: "Gel Lavanda",
+    imageUrl: "/assets/img/gel.png"
+  },
+  {
+    id: 6,
+    title: "Sabonete Líquido",
+    imageUrl: "/assets/img/sabonete-liquido.png"
+  },
+  {
+    id: 7,
+    title: "Amaciante Amor 1L",
+    imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 1L.png"
+  },
+  {
+    id: 8,
+    title: "Amaciante Amor 2L",
+    imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 2L.png"
+  },
+  {
+    id: 9,
+    title: "Amaciante Amor 3L",
+    imageUrl: "/assets/img/TEIÚ - AMACIANTE AMOR 3L.png"
+  },
+  {
+    id: 10,
+    title: "Lava Louças Maçã 500ML",
+    imageUrl: "/assets/img/TEIÚ - LAVA LOUÇAS MAÇÃ 500ML.png"
+  },
+  {
+    id: 11,
+    title: "Lava Louças Maçã 2L",
+    imageUrl: "/assets/img/TEIÚ - LAVA LOUÇAS MAÇÃ 2L.png"
+  },
+  {
+    id: 12,
+    title: "Lava Roupas Líquido Perfumes 1L",
+    imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 1L.png"
+  },
+  {
+    id: 13,
+    title: "Lava Roupas Líquido Perfumes 3L",
+    imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 3L.png"
+  },
+  {
+    id: 14,
+    title: "Lava Roupas Líquido Perfumes 5L",
+    imageUrl: "/assets/img/TEIÚ - LAVA ROUPAS LÍQUIDO PERFUMES 5L.png"
+  },
+  {
+    id: 15,
+    title: "Limpador Perfumado Felicitá 500ML",
+    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO FELICITÁ 500ML.png"
+  },
+  {
+    id: 16,
+    title: "Limpador Perfumado Felicitá 1L",
+    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO FELICITÁ 1L.png"
+  },
+  {
+    id: 17,
+    title: "Limpador Perfumado Intimitá 500ML",
+    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO INTIMITÁ 500ML.png"
+  },
+  {
+    id: 18,
+    title: "Limpador Perfumado Intimitá 1L",
+    imageUrl: "/assets/img/TEIÚ - LIMPADOR PERFUMADO INTIMITÁ 1L.png"
+  }
+];

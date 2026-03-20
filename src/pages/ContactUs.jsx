fbc61f88d8c596648f2aa90186890f8600a0b9dc
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-03-20 13:55:31 -0300

    feat: (página contact) cria pági

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,18 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import BannerContactSession from "../sections/ContactUs/BannerContact";
+import ContactForm from "../sections/ContactUs/ContactForm";
+
+function ConatctUs() {
+    return (
+    <>
+    <Navbar/>
+    <BannerContactSession/>
+    <ContactForm/>
+    <Footer/>
+
+    </>
+    );
+}
+
+export default ConatctUs;

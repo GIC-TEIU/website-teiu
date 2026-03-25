c0b562dec458fe7432d5b0961f9f77aa2af53d25
Ana Luiza Vasconcelos <desenvolvimento.software2@teiu.com.br>
2026-03-25 17:59:56 -0300

    style[ProductCard]: faz ajustes de estilo e altera nome de variaveis adaptando ao json

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
-const ProductCard = ({ title, image, parentId, variantId }) => {
+const ProductCard = ({ product }) => {
+    const displayVariant = product.variants?.find(v => v.featured) || product.variants?.[0];
+
+    if (!displayVariant) return null;
+
     return (
         <div className="group relative bg-[#E7EBEF] bg-card-teiu-watermark rounded-[35px] pt-8 pb-12 px-6 flex flex-col items-center justify-between 
                 shadow-[0_20px_50px_rgba(0,0,0,0.2)] hover:shadow-[0_30px_60px_rgba(0,0,0,0.25)] 
@@ -9,23 +13,25 @@ const ProductCard = ({ title, image, parentId, variantId }) => {
 
             <div className="w-full flex justify-center mb-5 h-[220px] overflow-visible relative z-10">
                 <img
-                    src={image}
-                    alt={title}
+                    src={displayVariant.image}
+                    alt={product.title}
                     className="h-full w-auto object-contain 
                                transition-transform duration-500 ease-in-out 
                                group-hover:scale-110
-                               filter drop-shadow-[8px_15px_15px_rgba(0,0,0,0.4)]"
+                               filter drop-shadow-[8px_15px_15px_rgba(0,0,0,0.2)]"
                 />
             </div>
 
-            <h3 className="relative z-10 text-[#003366] text-xl font-bold text-center leading-tight w-full">
-                {title}
-            </h3>
+            {product.title}
+              <span className="block text-sm font-medium opacity-60 mt-1 uppercase">
+                {product.volume}
+                </span>
+
                 <Link 
-                to={`/produto/${parentId}`} 
-                state={{ variantId }}
+                to={`/produto/${product.id}`}
+                state={{ variantId: displayVariant.id }}
                 className="absolute -bottom-4 left-6 bg-[#009FE3] hover:bg-[#0096ce] text-white font-bold py-2 px-6 rounded-full text-sm shadow-lg transition-transform hover:scale-105 whitespace-nowrap"
-            >
+                >
                 Saiba mais
             </Link>
         </div>

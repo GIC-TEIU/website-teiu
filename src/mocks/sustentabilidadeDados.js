eea13ea368bae72538cac572700256a0ebbb0f0f
Ana Luiza Vasconcelos <desenvolvimento.software2@teiu.com.br>
2026-03-30 09:50:13 -0300

    test: modifica cores da roda

--- src/mocks/sustentabilidadeDados.js
+++ src/mocks/sustentabilidadeDados.js
@@ -1,11 +1,11 @@
-import { Leaf, Recycle, Wind, Droplets } from "lucide-react"; // Importando ícones de exemplo
+import { Leaf, Recycle, Wind, Droplets } from "lucide-react";
 
 export const sustentabilidadeItems = [
   {
     id: 1,
     id_name: "inovacao",
     icon: Leaf, 
-    color: "#60944F", 
+    color: "rgb(72, 124, 54)", 
     color_light: "#84B075", 
     title: "Inovação e Produtos",
     description: "Desenvolver soluções, serviços ou produtos com menor impacto ambiental e maior benefício social."
@@ -14,8 +14,8 @@ export const sustentabilidadeItems = [
     id: 2,
     id_name: "circular",
     icon: Recycle,
-    color: "#84B075", 
-    color_light: "#A3C796", 
+    color: "#435815", 
+    color_light: "#435815", 
     title: "Economia Circular",
     description: "Maximizar o uso de recursos recicláveis e reutilizáveis, reduzindo o desperdício em toda a cadeia de produção."
   },
@@ -23,8 +23,8 @@ export const sustentabilidadeItems = [
     id: 3,
     id_name: "energia",
     icon: Wind,
-    color: "#A3C796", 
-    color_light: "#C0DDB6", 
+    color: " #5d7c1e", 
+    color_light: "#5d7c1e", 
     title: "Energia Renovável",
     description: "Implementar fontes de energia limpa (solar, eólica) para mover as fábricas e operações da Teiú."
   },
@@ -32,8 +32,8 @@ export const sustentabilidadeItems = [
     id: 4,
     id_name: "agua",
     icon: Droplets,
-    color: "#C0DDB6", 
-    color_light: "#60944F",
+    color: "rgb(92, 117, 44)", 
+    color_light: "#31460a",
     title: "Gestão Hídrica",
     description: "Utilizar tecnologias avançadas para reuso de água e tratamento eficiente de efluentes industriais."
   },

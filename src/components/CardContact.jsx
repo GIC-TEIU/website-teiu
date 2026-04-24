83ada5b977a3484fe9263a105b1d68c475862959
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-04-24 16:38:25 -0300

    refactor (sessions): torna cards clicáveis

--- src/components/CardContact.jsx
+++ src/components/CardContact.jsx
@@ -1,17 +1,44 @@
-function CardContact({ icon, title, description }) {
-    return (
-      <div className="bg-black/10 backdrop-blur-md p-5 rounded-xl w-full sm:w-64 text-white border border-white/20">
-        <img src={icon} alt={title} />
-        
-        <h3 className="font-semibold mt-6">
-          {title}
-        </h3>
-        
-        <p className="text-sm opacity-80">
-          {description}
-        </p>
-      </div>
-    );
-  }
-  
-  export default CardContact;
+function CardContact({ icon, title, description, action }) {
+
+  const handleClick = () => {
+    let url = "#";
+
+    switch (action) {
+      case "instagram":
+        url = "https://instagram.com/teiuoficial";
+        break;
+
+      case "whatsapp":
+        url = "https://wa.me/557799137026"; 
+        break;
+
+      case "email":
+        url = "https://mail.google.com/mail/?view=cm&to=desenvolvimento.web2@teiu.com.br";
+        break;
+
+      default:
+        return;
+    }
+
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className="cursor-pointer bg-black/10 backdrop-blur-md p-5 rounded-xl w-full sm:w-64 text-white border border-white/20 hover:scale-105 transition"
+    >
+      <img src={icon} alt={title} />
+      
+      <h3 className="font-semibold mt-6">
+        {title}
+      </h3>
+      
+      <p className="text-sm opacity-80">
+        {description}
+      </p>
+    </div>
+  );
+}
+
+export default CardContact;

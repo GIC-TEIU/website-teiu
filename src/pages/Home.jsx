4ae1765d50bba1555b2fe737528219415791a453
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-04-24 16:26:13 -0300

    feat(home): integra home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,49 @@
+import { useEffect, useState } from "react";
 
-import ProductsHome from "../sections/Home/ProductsHome"
-import Hero from "../sections/Home/Hero"
-import Footer from "../components/Footer"
-import InstitutionalVideo from "../sections/Home/VideoPage"
-import InstagramSection from "../sections/Home/InstagramSection"
-import Navbar from "../components/Navbar"
+import ProductsHome from "../sections/Home/ProductsHome";
+import Hero from "../sections/Home/Hero";
+import Footer from "../components/Footer";
+import InstitutionalVideo from "../sections/Home/VideoPage";
+import InstagramSection from "../sections/Home/InstagramSection";
+import Navbar from "../components/Navbar";
 
 function Home() {
-    return ( 
-        <>
-        <Navbar/>
-        <main className="w-full">
-          <Hero />
-          <ProductsHome />
-          <InstitutionalVideo />
-          <div className="relative w-full bg-unificado-teiu overflow-hidden">
-          <InstagramSection />
-          <Footer/>
-          </div>
-        </main>
-      </>
-     );
+  const [pageData, setPageData] = useState(' ');
+
+  useEffect(() => {
+    const fetchPage = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/pageWebsite/home");
+        const data = await response.json();
+        console.log(data)
+        setPageData(data);
+      } catch (error) {
+        console.error("Erro ao buscar página:", error);
+      }
+    };
+
+    fetchPage();
+  }, []);
+  useEffect(()=> {
+    console.log('aaaaaaaaaaaaaaaaaa',pageData?.components?.banner?.assets?.banner_home?.url)
+  },[pageData])
+
+  return (
+    <>
+      <Navbar />
+
+      <main className="w-full">
+        <Hero data={pageData} />
+        <ProductsHome data={pageData} />
+        <InstitutionalVideo data={pageData} />
+
+        <div className="relative w-full bg-unificado-teiu overflow-hidden">
+          <InstagramSection data={pageData} />
+          <Footer data={pageData} />
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Home;

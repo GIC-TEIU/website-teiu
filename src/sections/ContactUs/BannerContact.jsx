be5d23b48c8c3af759299198c0b18bb408aa2e36
Ícaro Rodrigues <desenvolvimento.web2@teiu.com.br>
2026-05-07 10:59:34 -0300

    feat(sections): integra backend com front da página de contatos

--- src/sections/ContactUs/BannerContact.jsx
+++ src/sections/ContactUs/BannerContact.jsx
@@ -1,55 +1,56 @@
 import CardContact from "../../components/CardContact";
 import Navbar from "../../components/Navbar";
-function BannerContactSession() {
+
+function BannerContactSession({ data }) {
+  const API_URL = "http://127.0.0.1:8080";
+
+
+  console.log("BannerContactSession data:", data);
+
   return (
     <div className="w-full h-[90vh] relative">
       <Navbar />
+
       {/* Background */}
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage:
-            "url('/assets/img/banner-contact.jpeg')", // troca pelo caminho da sua imagem
+          backgroundImage: "url('/assets/img/banner-contact.jpeg')",
         }}
       />
 
-
-      <div className="absolute inset-0" />
+      <div className="absolute inset-0  " />
 
       {/* Conteúdo */}
       <div className="relative z-10 flex flex-col justify-center h-full px-6 sm:px-10 lg:px-70">
-        {/* Título */}
+
+       
         <h1 className="text-white text-3xl sm:text-5xl font-semibold max-w-xl leading-tight mt-40 mb-10">
           PRECISA FALAR <br /> COM A TEIU?
         </h1>
 
-        {/* Cards */}
         <div className="flex flex-col sm:flex-row gap-6">
-          
-        <CardContact
-        icon="/assets/img/email.svg"
-        title="Por Email"
-        description="teiu@teiu.com.br"
-        action="email"
-        
-        />
 
-        <CardContact
-        icon="/assets/img/wpp.svg"
-        title="No WhatsApp"
-        description="(00) 00000-0000"
-        action="whatsapp"
+          <CardContact
+            icon={`${API_URL}/storage/${data?.assets?.email_icon?.url}`}
+            title={data?.texts?.email_title?.content}
+            description={data?.texts?.email_value?.content}
+            action="email"
+          />
 
-        />
+          <CardContact
+            icon={`${API_URL}/storage/${data?.assets?.email_icon?.url}`}
+            title={data?.texts?.whatsapp_title?.content}
+            description={data?.texts?.whatsapp_value?.content}
+            action="whatsapp"
+          />
 
-        <CardContact
-        icon="/assets/img/instagram.svg"
-        title="Nas Redes"
-        description="@teiuoficial"
-        action="instagram"
-        />
-
-         
+          <CardContact
+            icon={`${API_URL}/storage/${data?.assets?.email_icon?.url}`}
+            title={data?.texts?.social_title?.content}
+            description={data?.texts?.social_value?.content}
+            action="instagram"
+          />
 
         </div>
       </div>
